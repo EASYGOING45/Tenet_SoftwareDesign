8ab61f1d8e2e79eadb89e0df29f35844b700dcae
EASTGOING45 <1505674928@qq.com>
2023-10-27 19:58:27 +0800

    Transaction.atomic

--- BKAdmin/ExamSystem/designView.py
+++ BKAdmin/ExamSystem/designView.py
@@ -196,3 +196,127 @@ def deleteWj(info,username):
         response['code'] = '-3'
         response['msg'] = '确少必要参数'
     return response
+
+
+"""
+获取问题列表
+"""
+def getQuestionList(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')#wjid
+    if username:
+        res=Wj.objects.filter(id=wjId,username=username)
+        if res.exists():#判断该问卷id是否为本人创建
+            obj=Question.objects.filter(wjId=wjId)
+            detail=[]
+            for item in obj:
+                temp={}
+                temp['title']=item.title
+                temp['type']=item.type
+                temp['id']=item.id#问题id
+                temp['row']=item.row
+                temp['must']=item.must
+                #获取选项
+                temp['options']=[]
+                if temp['type'] in ['radio', 'checkbox']:  # 如果是单选或者多选
+                    optionItems = Options.objects.filter(questionId=item.id)
+                    for optionItem in optionItems:
+                        temp['options'].append({'title': optionItem.title, 'id': optionItem.id})
+                temp['radioValue']=-1#接收单选框的值
+                temp['checkboxValue'] =[]#接收多选框的值
+                temp['textValue']=''#接收输入框的值
+                detail.append(temp)
+            response['detail']=detail
+        else:
+            response['code'] = '-6'
+            response['msg'] = '权限不足'
+
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
+
+
+"""
+添加问题/更新问题
+当传入quetionId为空时->添加问题 不为空时->更新问题
+事务处理：当一次插入选项过多时会浪费时间，增加事务处理可大大加快速度（改进后20个选项3秒插入完成）
+"""
+# @transaction.atomic 是事务装饰器 用于事务处理 保证数据的完整性
+@transaction.atomic
+def addQuestion(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    wjId=info.get('wjId')#wjid
+    q_title=info.get('title')#题目标题
+    q_type=info.get('type')#题目类型
+    options=info.get('options')#选项
+    row=info.get('row')
+    must=info.get('must')
+    questionId=info.get('questionId')#问题id 可为空
+    if wjId and q_title and q_type and must!=None:
+        if q_type in ['radio','checkbox','text']:
+            if questionId:#问题id存在 更新问题
+                newIds=[]
+                for temp in options:
+                    newIds.append(temp['id'])#将更新后的选项id记录
+                allOptions=Options.objects.filter(questionId=questionId)
+                #遍历选项 把不在更新后的选项id中的选项删除
+                for option in allOptions:
+                    if option.id not in newIds:
+                        option.delete()
+                #更新问题
+                Question.objects.filter(wjId=wjId,id=questionId).update(title=q_title,type=q_type,must=must,row=row)
+                #更新选项
+                for option in options:
+                    if option['id']!=0:#选项为已有的 更新
+                        Options.objects.filter(questionId=questionId, id=option['id']).update(title=option['title'])
+                    else:#选项为新增的 添加
+                        Options.objects.create(questionId=questionId,title=option['title'])
+            else:#问题id不存在 添加问题
+                # 添加问题
+                resObj = Question.objects.create(wjId=wjId, title=q_title, type=q_type, row=row,must=must)
+                questionId = resObj.id
+                response['id'] = questionId
+                # 添加选项
+                if q_type == 'radio' or q_type == 'checkbox':  # 单选或者多选
+                    print(type(options))
+                    if options and type(options) == type([]):
+                        for item in options:
+                            Options.objects.create(questionId=questionId, title=item['title'])
+                            # Options(questionId=questionId,title=item)
+                    else:  # 传入选项不能为空
+                        response['code'] = '-4'
+                        response['msg'] = '操作失败'
+        else:
+            response['code'] = '-5'
+            response['msg'] = '传入参数值有误'
+            return response
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
+
+"""
+删除问题
+"""
+@transaction.atomic
+def deleteQuestion(info,username):
+    response = {'code': 0, 'msg': 'success'}
+    questionId=info.get('questionId')
+    if questionId and username:
+        try:
+            s_wjId=Question.objects.get(id=questionId).wjId#该题目所属的问卷id
+            s_username=Wj.objects.get(id=s_wjId).username#该题目所属的用户名
+            if username==s_username:#该题目是此用户创建的 有权限删除
+                Question.objects.filter(id=questionId).delete()#删除问题
+                Options.objects.filter(questionId=questionId).delete()#删除关联选项
+            else:#该题目不是此用户创建的 无权限删除
+                response['code'] = '-6'
+                response['msg'] = '权限不足'
+        except:
+            response['code'] = '-4'
+            response['msg'] = '操作失败'
+    else:
+        response['code'] = '-3'
+        response['msg'] = '确少必要参数'
+    return response
