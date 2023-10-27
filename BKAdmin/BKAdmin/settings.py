6a1c058f9c10ab97374f546ffcbbbf0a4c55960a
EASTGOING45 <1505674928@qq.com>
2023-10-27 16:25:20 +0800

    Settings.py

--- BKAdmin/BKAdmin/settings.py
+++ BKAdmin/BKAdmin/settings.py
@@ -25,25 +25,27 @@ SECRET_KEY = 'pp@iq9)!j579wb@0%o22i)1=49p5x!1!x9hd-@1)6(0yp)7gj^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ExamSystem.apps.ExamsystemConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'BKAdmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ExamSystem/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +75,17 @@ WSGI_APPLICATION = 'BKAdmin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import pymysql 
+pymysql.install_as_MySQLdb()  # 配置MySQL数据库
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bk_examsystem',
+        'USER': 'root',
+        'PASSWORD': 'huan0912',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -103,18 +112,23 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
+SIMPLEUI_ANALYSIS = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "ExamSystem/dist/static"),
+]
--- BKAdmin/ExamSystem/apps.py
+++ BKAdmin/ExamSystem/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class ExamsystemConfig(AppConfig):
-    name = 'ExamSystem'
+    name = 'ExamSystem' # 这个是在app中显示的名称
+    verbose_name = '问卷系统'  # 这个是在admin站点中显示的名称
