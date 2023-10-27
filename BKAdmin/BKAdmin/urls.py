1768766f069ddc934638c7cb325a90bf244b87f2
EASTGOING45 <1505674928@qq.com>
2023-10-27 20:33:19 +0800

    URL Config

--- BKAdmin/BKAdmin/urls.py
+++ BKAdmin/BKAdmin/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from ExamSystem import designView,answerView
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'admin/', admin.site.urls),
+    url('^api/design',designView.opera),#问卷设计者操作
+    url('^api/answer',answerView.opera),#问卷回答者操作
+    url(r'home/', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'index/', TemplateView.as_view(template_name="index.html")),
+    url(r'login/', TemplateView.as_view(template_name="index.html")),
+    url(r'register/', TemplateView.as_view(template_name="index.html")),
+    url(r'resetpass/', TemplateView.as_view(template_name="index.html")),
+    url(r'^display.*$', TemplateView.as_view(template_name="index.html")),
+    url(r'thankyou/', TemplateView.as_view(template_name="index.html")),
+
+]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
